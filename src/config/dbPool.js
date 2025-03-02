6322610336e1baa30259b7e7a23ff5d520cb6a96
Hanmingyu02 <hmg0835@naver.com>
2025-03-03 00:21:30 +0900

    dbPool 완성

--- src/config/dbPool.js
+++ src/config/dbPool.js
@@ -1,12 +1,15 @@
-// models/dbPool.js
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 const pool = mysql.createPool({
-    host: 'localhost',
-    port: '3306',
-    user: 'scott',
-    password: 'tiger',
-    database: 'edudb',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'edudb',
     connectionLimit: 10,
     waitForConnections: true,
+    queueLimit: 0
 });
+
 module.exports = pool;
