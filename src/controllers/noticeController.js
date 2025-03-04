9e8d035cb967d6bc319e6742e65b141e17fbe05a
Hanmingyu02 <hmg0835@naver.com>
2025-03-05 02:20:52 +0900

    공지사항 수정 삭제 완료

--- src/controllers/noticeController.js
+++ src/controllers/noticeController.js
@@ -25,6 +25,35 @@ exports.createNotice = async (req, res) => {
         res.status(500).json({ result: "fail", message: "DB 에러 발생: " + err.message });
     }
 };
-exports.updateNotice = async (req, res) => {};
-exports.deleteNotice = async (req, res) => {};
+exports.updateNotice = async (req, res) => {
+    const { notice_id } = req.params;
+    const { title, content } = req.body;
+
+    try {
+        const noticeData = [title, content, notice_id];
+        const sql = `update notices set title=?, content=? where notice_id=?`;
+        const [result] = await pool.query(sql, noticeData);
+        if (result === 0) {
+            return res.json({ result: "fail", message: "공지사항 수정 실패" });
+        } else {
+            return res.json({ result: "success", message: "공지사항 수정 성공" });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+exports.deleteNotice = async (req, res) => {
+    const { notice_id } = req.params;
+    try {
+        const sql = "delete from notices where notice_id=?";
+        const [result] = await pool.query(sql, notice_id);
+        if (result.affectedRows === 0) {
+            return res.json({ result: "fail", message: "삭제할 공지사항이 존재하지 않음" });
+        } else {
+            res.json({ result: "success", message: "공지사항 삭제 성공" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 exports.searchNotices = async (req, res) => {};
