69d5338f621e2a9a73c005b00f9e178fc37eed3a
Hanmingyu02 <hmg0835@naver.com>
2025-03-05 02:10:06 +0900

    공지사항 조회 등록 완성

--- src/controllers/noticeController.js
+++ src/controllers/noticeController.js
@@ -1,3 +1,30 @@
-const pool = require('../config/dbPool');
+const pool = require("../config/dbPool");
+require("dotenv").config();
 
-require('dotenv').config();
+exports.getNotices = async (req, res) => {
+    const sql = `select * from notices order by user_id desc`;
+    try {
+        const [result] = await pool.query(sql);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ message: err.message });
+    }
+};
+exports.createNotice = async (req, res) => {
+    const [title, content] = req.body;
+    const noticeData = [title, content];
+    try {
+        const sql = `insert into notices(title, content) values(?,?)`;
+        const [result] = await pool.query(sql, noticeData);
+        if (result.affectedRows > 0) {
+            res.json({ result: "success", message: "공지사항 등록 성공" });
+        } else {
+            res.json({ result: "fail", message: "공지사항 등록 실패" });
+        }
+    } catch (error) {
+        res.status(500).json({ result: "fail", message: "DB 에러 발생: " + err.message });
+    }
+};
+exports.updateNotice = async (req, res) => {};
+exports.deleteNotice = async (req, res) => {};
+exports.searchNotices = async (req, res) => {};
--- src/routes/noticeRoutes.js
+++ src/routes/noticeRoutes.js
@@ -8,9 +8,13 @@ const {
     searchNotices,
 } = require("../controllers/noticeController");
 // 게시글 조회
-
+router.get("/", getNotices);
 // 게시글 작성
+router.post("/", createNotice);
 // 게시글 수정
+router.post("/:notice_id", updateNotice);
 // 게시글 삭제
+router.delete("/:notice_id", deleteNotice);
 // 게시글 검색
+router.get("/search", searchNotices);
 module.exports = router;
